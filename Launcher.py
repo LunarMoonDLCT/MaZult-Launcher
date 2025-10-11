66fe4a34b62c35053ad4f173f32952704ba5dff4
LunarMoonDLCT <levinhtoan8116pc@gmail.com>
2025-10-11 14:05:09 +0700

    yea bug fix :D

--- Launcher.py
+++ Launcher.py
@@ -32,7 +32,7 @@ from pypresence.exceptions import InvalidID, PipeClosed
 from packaging import version as packaging
 
 
-LAUNCHER_VERSION = "1.105.28.3"
+LAUNCHER_VERSION = "1.105.28.4"
 GITHUB_API_URL = "https://api.github.com/repos/LunarMoonDLCT/MaZult-Launcher/releases/latest"
 DISCORD_CLIENT_ID = "1410269369748946986"
 
